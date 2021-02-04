d2c90ab4f323cbc05589606009b179ef150f7199
Abhijay007j <Abhijay007j@gmail.com>
2021-02-04 12:58:29 +0530

    Aligned dark mode button to bottom left corner

--- app.js
+++ app.js
@@ -7,26 +7,6 @@ const ejs = require("ejs");
 const mongoose = require('mongoose');
 const _ = require("lodash");
 const PORT = process.env.PORT || 3000;
-//darkmode
-const Darkmode = require("darkmode-js") ;
-
-
-const options = {
-  bottom: '64px', // default: '32px'
-  right: 'unset', // default: '32px'
-  left: '32px', // default: 'unset'
-  time: '0.5s', // default: '0.3s'
-  mixColor: '#fff', // default: '#fff'
-  backgroundColor: '#fff',  // default: '#fff'
-  buttonColorDark: '#100f2c',  // default: '#100f2c'
-  buttonColorLight: '#fff', // default: '#fff'
-  saveInCookies: false, // default: true,
-  label: '🌗', // default: ''
-  autoMatchOsTheme: true // default: true
-}
-
-const darkmode = new Darkmode(options);
-darkmode.showWidget();
 
 
 //Default Texts-
