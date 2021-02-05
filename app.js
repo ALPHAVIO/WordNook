ad5beeafc6dd90608c588c8be22b3c36c3efaa92
Monojit Chakraborty <cmonojit1999@gmail.com>
2021-02-05 19:40:01 +0530

    style-modified all fonts
    Modified the fonts of Home page, header, footer, Post preview, Post, About page and Contact page.

--- app.js
+++ app.js
@@ -46,8 +46,11 @@ if(process.env.NODE_ENV !== 'production'){
   dotenv.config({path:'./.env'});
 }
 
+
 //Connecting to Mongo Database using ODM Mongoose-
+
 const URL = process.env.URL;
+// const URL = 'mongodb://localhost:27017/blogDB';
 mongoose.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true});
 
 //Setting up schema for the collection-
