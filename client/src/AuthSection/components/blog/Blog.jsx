42bc122dfff22c2656c472c52650a85fc63b4110
A7abhilash <a7abhilash@gmail.com>
2021-03-06 18:19:57 +0530

    added custom error component

--- client/src/AuthSection/components/blog/Blog.jsx
+++ client/src/AuthSection/components/blog/Blog.jsx
@@ -7,6 +7,7 @@ import UserProfile from "../../containers/UserProfile";
 import { useAuth } from "../../../contexts/AuthContext";
 import { useAlert } from "../../../contexts/AlertContext";
 import Loader from "../../../containers/Loader";
+import ErrorBox from "../error/ErrorBox";
 
 function Blog(props) {
   const { user } = useAuth();
@@ -44,54 +45,57 @@ function Blog(props) {
   return loading ? (
     <Loader height="50" />
   ) : (
-    !error && blog && (
-      <div className="row">
-        <motion.div
-          variants={blogAuthorVariant}
-          initial="hidden"
-          animate="visible"
-          exit="exit"
-          className="col-md-4 my-2"
-        >
-          <h4 className="text-center text-danger">Author</h4>
-          <UserProfile user={blog.author} />
-          <Link
-            to={`${
-              blog.author._id !== user._id
-                ? `/author/${blog.author._id}`
-                : "/dashboard"
-            }`}
-            className="btn btn-info btn-sm btn-block my-2"
+    <>
+      {error && <ErrorBox />}
+      {!error && blog && (
+        <div className="row">
+          <motion.div
+            variants={blogAuthorVariant}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            className="col-md-4 my-2"
           >
-            Visit Profile
-          </Link>
-        </motion.div>
-        <motion.div
-          variants={blogContentVariant}
-          initial="hidden"
-          animate="visible"
-          exit="exit"
-          className="col-md-7 mx-2 p-2 my-2 mx-md-auto bg-dark"
-        >
-          <h3 className="text-center text-warning mb-1">{blog.blogTitle}</h3>
-          <p className="pb-1 text-muted text-center border-bottom border-secondary">
-            <i className="fas fa-clock mr-1"></i>
-            <span>{new Date(blog.timestamps).toDateString()}</span>
-          </p>
-          <div
-            className="border-left border-danger m-2 p-2"
-            dangerouslySetInnerHTML={{ __html: blog.blogContent }}
+            <h4 className="text-center text-danger">Author</h4>
+            <UserProfile user={blog.author} />
+            <Link
+              to={`${
+                blog.author._id !== user._id
+                  ? `/author/${blog.author._id}`
+                  : "/dashboard"
+              }`}
+              className="btn btn-info btn-sm btn-block my-2"
+            >
+              Visit Profile
+            </Link>
+          </motion.div>
+          <motion.div
+            variants={blogContentVariant}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            className="col-md-7 mx-2 p-2 my-2 mx-md-auto bg-dark"
           >
-            {/* <ReactQuill
+            <h3 className="text-center text-warning mb-1">{blog.blogTitle}</h3>
+            <p className="pb-1 text-muted text-center border-bottom border-secondary">
+              <i className="fas fa-clock mr-1"></i>
+              <span>{new Date(blog.timestamps).toDateString()}</span>
+            </p>
+            <div
+              className="border-left border-danger m-2 p-2"
+              dangerouslySetInnerHTML={{ __html: blog.blogContent }}
+            >
+              {/* <ReactQuill
               className="bg-dark text-white"
               value={blog.body}
               readOnly={true}
               theme="bubble"
             /> */}
-          </div>
-        </motion.div>
-      </div>
-    )
+            </div>
+          </motion.div>
+        </div>
+      )}
+    </>
   );
 }
 
---
+++ client/src/AuthSection/components/error/ErrorBox.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import error from "./error.png";
+
+function ErrorBox() {
+  return (
+    <div className=" mx-2 mx-md-auto text-center">
+      <img
+        src={error}
+        alt="Error"
+        className="img-fluid my-5"
+        style={imgStyle}
+      />
+      <h5>Something went wrong!</h5>
+      <p>Head back & refresh to continue.</p>
+    </div>
+  );
+}
+
+export default ErrorBox;
+
+const imgStyle = {
+  height: "50vh",
+};
