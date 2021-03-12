35fd20bffcff6619babc16bd6298e86fa04dffd9
hrushikesh-git <hrushikeshrasne@gmail.com>
2021-03-13 00:22:14 +0530

    Change the color pallet of navbar

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ if(process.env.NODE_ENV !== 'production'){
 }
 
 //Connecting to Mongo Database using ODM Mongoose-
-const URL = process.env.URL;
-mongoose.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true});
+const URL = 'mongodb://localhost:27017/blogDB';
+mongoose.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex:true});
 
 //Setting up schema for the collection-
 const blogSchema = {
