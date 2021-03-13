68d614a95669769562c9ff7a4cf491edc4590b57
Athira.V.Ajit <mailathiravajit@gmail.com>
2021-03-13 20:13:17 +0530

    Issue #326

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ if (process.env.NODE_ENV !== 'production') {
 
 //Connecting to Mongo Database using ODM Mongoose-
 const URL = process.env.URL;
-
 mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Router for user login and sign in
