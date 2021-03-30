3f7347ae6b895b07f94543e8986b0a9462258d05
Rimpi <rimpiranibaruah123@gmail.com>
2021-03-30 21:42:46 +0530

    Added number of views in blogs

--- models/Blog.model.js
+++ models/Blog.model.js
@@ -1,20 +1,25 @@
-const mongoose = require('mongoose');
-const Schema  = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 //Setting up schema for the collection-
 const blogSchema = new Schema({
-    blogTitle: String,
-    blogContent: String,
-    comments: Array,
-    timestamps: {
-      type: Date,
-      default: Date.now
-    },
-    author: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User"
-    }
-  })
-  
-  //Making a MongoDB model for the schema-
-  module.exports = mongoose.model("Blog", blogSchema);
+  blogTitle: String,
+  blogContent: String,
+  comments: Array,
+  timestamps: {
+    type: Date,
+    default: Date.now,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+
+  noOfViews: {
+    type: Number,
+    default: 0,
+  },
+});
+
+//Making a MongoDB model for the schema-
+module.exports = mongoose.model("Blog", blogSchema);
